b76084bed98ca07b4fadbf7178c23a29f481a46f
ive-m <ivesita.maria@gmail.com>
2023-02-06 14:07:10 -0500

    Add 01_schema and 01_seeds in the db directory

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO categories (name) VALUES ('movie');
+INSERT INTO categories (name) VALUES ('book');
+INSERT INTO categories (name) VALUES ('product');
+INSERT INTO categories (name) VALUES ('restaurant');
+
+/*Insert fake data*/
+INSERT INTO tasks (name,category_id, start_date, end_date)
+VALUES
+('Avatar', 1,'2023-02-11', '2023-02-18' ),/*movie*/
+('It Ends with Us', 2,'2023-02-20', '2023-02-26' ),/*book*/
+('Johns Lunch', 4,'2023-02-14', '2023-02-15' ),/*restaurant*/
+('Book Lovers', 2,'2023-02-18', '2023-02-26' ),/*book*/
+('Bed', 3,'2023-03-10', '2023-03-20' ),/*product*/
+('La Fontaine', 4,'2023-04-10', '2023-04-30' ),/*restaurant*/
+('Jurassic World', 1,'2023-03-01', '2023-03-09' ),/*movie*/
+('A Heart That Works', 2,'2023-02-20', '2023-02-26' ),/*book*/
+('Watch Minions', 1,'2023-04-28', '2023-05-12' ),/*movie*/
+('Laptop', 3,'2023-02-15', '2023-03-01' ),/*product*/
+('Doctor Strange', 1,'2023-03-09', '2023-03-19' );/*movie*/
