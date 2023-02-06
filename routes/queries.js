73e3dcca03c6a27466037ee1551f822087f8c568
Jason Kadman <jkadman231@gmail.com>
2023-02-06 20:01:51 +0000

    play with routes

--- routes/queries.js
+++ routes/queries.js
@@ -9,7 +9,7 @@ const router  = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.render('books')
+  res.render('/')
 })
 
 
@@ -19,49 +19,50 @@ const generateRandomString = function() {
 };
 
 // sample database to store interests
-// const interestDatabase = {};
+const interestDatabase = {};
 
-// app.post('/index', (req, res) => {
+router.post('/queries', (req, res) => {
 
-// newId = generateRandomString();
+newId = generateRandomString();
 
-// const searchQuery = req.body.searching;
+const searchQuery = req.body.searching;
 
-// const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=a3a238db7e3932463061304b7f22746a&query=${searchQuery}`;
+const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=a3a238db7e3932463061304b7f22746a&query=${searchQuery}`;
 
-// const urlBook = `https://openlibrary.org/search.json?q=${searchQuery}`;
+const urlBook = `https://openlibrary.org/search.json?q=${searchQuery}`;
 
 
-// const request1 = axios.get(urlMovie);
-// const request2 = axios.get(urlBook)
+const request1 = axios.get(urlMovie);
+const request2 = axios.get(urlBook)
 
-// newArray = [];
+newArray = [];
 
-// request1.then((response) => {
-//   newArray.push(response.data.results)
-//   // console.log(response.data.results)
-// })
-// request2.then((response) => {
-//   // console.log(response.data.docs)
-//   newArray.push(response.data.docs)
-// })
-// .then((response) => {
-//   if (newArray.length > 0) {
+request1.then((response) => {
+  newArray.push(response.data.results)
+  // console.log(response.data.results)
+})
+request2.then((response) => {
+  // console.log(response.data.docs)
+  newArray.push(response.data.docs)
+})
+.then((response) => {
+  if (newArray.length > 0) {
 
-//     interestDatabase = {
-//       newId: searchQuery
-//     };
+    interestDatabase = {
+      newId: searchQuery
+    };
 
-//     // console.log(newArray)
+    console.log(interestDatabase)
+    // console.log(newArray)
 
-//   } else {
+  } else {
 
-//     console.log('no beano')
-//   }
-// })
+    console.log('nope')
+  }
+})
 
-// res.redirect('/temp')
-// })
+res.send('Ok')
+})
 
 
 
