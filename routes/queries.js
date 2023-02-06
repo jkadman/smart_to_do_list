c12767fa4622c37c4358bfc46239f2899f718fcd
Jason Kadman <jkadman231@gmail.com>
2023-02-06 17:48:15 +0000

    try to get ejs files to render

--- routes/queries.js
+++ routes/queries.js
@@ -1,30 +1,67 @@
-const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=a3a238db7e3932463061304b7f22746a&query=${searchQuery2}`;
+const axios = require('axios');
+const express = require('express');
+const router  = express.Router();
 
-const urlBook = `https://openlibrary.org/search.json?q=${searchQuery2}`;
 
 
-const request1 = axios.get(urlMovie);
-const request2 = axios.get(urlBook)
-
-newArray = [];
-
-request1.then((response) => {
-  newArray.push(response.data.results)
-  // console.log(response.data.results)
+router.get('/books', (req, res) => {
+  res.render('books')
 })
-request2.then((response) => {
-  // console.log(response.data.docs)
-  newArray.push(response.data.docs)
-})
-.then((response) => {
-  if (newArray.length > 0) {
 
-    console.log('it works!')
 
-    // console.log(newArray)
+// function to generate random string
+const generateRandomString = function() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+};
 
-  } else {
+// sample database to store interests
+// const interestDatabase = {};
 
-    console.log('no beano')
-  }
-})
+// app.post('/index', (req, res) => {
+
+// newId = generateRandomString();
+
+// const searchQuery = req.body.searching;
+
+// const urlMovie = `https://api.themoviedb.org/3/search/movie?api_key=a3a238db7e3932463061304b7f22746a&query=${searchQuery}`;
+
+// const urlBook = `https://openlibrary.org/search.json?q=${searchQuery}`;
+
+
+// const request1 = axios.get(urlMovie);
+// const request2 = axios.get(urlBook)
+
+// newArray = [];
+
+// request1.then((response) => {
+//   newArray.push(response.data.results)
+//   // console.log(response.data.results)
+// })
+// request2.then((response) => {
+//   // console.log(response.data.docs)
+//   newArray.push(response.data.docs)
+// })
+// .then((response) => {
+//   if (newArray.length > 0) {
+
+//     interestDatabase = {
+//       newId: searchQuery
+//     };
+
+//     // console.log(newArray)
+
+//   } else {
+
+//     console.log('no beano')
+//   }
+// })
+
+// res.redirect('/temp')
+// })
+
+
+
+// router.post('/', (req, res) => {
+//   console.log(req.body); // Log the POST request body to the console
+//   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// })
