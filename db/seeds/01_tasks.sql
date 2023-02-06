05d873a3b5da91009cdeaae3fb8fb8e66d4b43ea
ive-m <ivesita.maria@gmail.com>
2023-02-06 15:33:56 -0500

    Separate files per each table

--- db/seeds/01_tasks.sql
+++ db/seeds/01_tasks.sql
@@ -1,7 +1,3 @@
-INSERT INTO categories (name) VALUES ('movie');
-INSERT INTO categories (name) VALUES ('book');
-INSERT INTO categories (name) VALUES ('product');
-INSERT INTO categories (name) VALUES ('restaurant');
 
 /*Insert fake data*/
 INSERT INTO tasks (name,category_id, start_date, end_date)
